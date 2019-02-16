51a6ecc536c80e83e4a14c9b3818d3538af3d9ab
vsapiens <erick.frank@icloud.com>
2019-02-16 02:14:25 -0600

    Person updated in Azure

--- DDL.sql
+++ DDL.sql
@@ -1,7 +1,7 @@
 BEGIN
 
-REATE TABLE Person (
-person_id INT AUTO_INCREMENT,
+CREATE TABLE Person (
+person_id INT identity(1,1),
 first_name VARCHAR(20) NOT NULL,
 last_name VARCHAR(20) NOT NULL,
 nickname VARCHAR(20) NOT NULL,
@@ -10,10 +10,18 @@ phone VARCHAR(15) NOT NULL,
 city VARCHAR(20) NOT NULL,
 country VARCHAR(20) NOT NULL,
 email VARCHAR(30) NOT NULL,
-initial_date DATE NOT NULL,
-PRIMARY KEY(person_id)
-);
+initial_date DATE NOT NULL
 
+CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED
+(
+    person_id ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]        
+) ON [PRIMARY] 
+
+INSERT INTO Person(first_name, last_name,nickname,sex, phone, city, country, email, initial_date ) VALUES ('ERICK','GONZALEZ','vsapiens','M','8120008400','Monterrey','Mexico','erick.frank@icloud.com', '2019-02-16')
+
+SELECT * FROM Person
+SELECT * FROM Person
 CREATE TABLE Project(
 project_id INT AUTO_INCREMENT,
 name VARCHAR(40) NOT NULL,
