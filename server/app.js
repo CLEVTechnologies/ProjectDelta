1cf88274046dceed9525e1c1bf46360152bbe700
Carlos Estrada <carlos.deg02@hotmail.com>
2019-02-19 14:03:08 -0600

    Added routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,24 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 
 var projectRoutes = require("./routes/projects");
+
+var con = mysql.createConnection({
+    host: "10.23.14.11",
+    port:  "3306",
+    user: "root",
+    password: "rootpass",
+    database: "Delta"
+});
+
+con.connect((err) => {
+    if (err) {
+        console.log("PEEEP");
+    } else {
+        console.log("Connected!!!");
+    }
+});
 
 app.use("/api/projects", projectRoutes);
 
